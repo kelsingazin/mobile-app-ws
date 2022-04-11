6358641403448f53daac9cc28969c52eabdafa61
kelsingazin <kelsingaziny@gmail.com>
2022-04-11 15:29:38 +0600

    Creating AddressDto

---
+++ src/main/java/com/appsdeveloperblog/app/ws/shared/dto/AddressDto.java
@@ -0,0 +1,11 @@
+package com.appsdeveloperblog.app.ws.shared.dto;
+
+public class AddressDto {
+    public long id;
+    private String city;
+    private String country;
+    private String streetName;
+    private String postalCode;
+    private String type;
+    private UserDto userDetails;
+}
