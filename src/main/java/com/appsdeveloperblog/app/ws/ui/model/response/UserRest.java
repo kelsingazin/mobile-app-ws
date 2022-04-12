eb90c1fb912537948961af35cecebebdb343ec2f
kelsingazin <kelsingaziny@gmail.com>
2022-04-12 20:22:09 +0600

    Include list of addresses into response

--- src/main/java/com/appsdeveloperblog/app/ws/ui/model/response/UserRest.java
+++ src/main/java/com/appsdeveloperblog/app/ws/ui/model/response/UserRest.java
@@ -2,10 +2,13 @@ package com.appsdeveloperblog.app.ws.ui.model.response;
 
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 public class UserRest {
     private String userId;
     private String firstName;
     private String lastName;
     private String email;
+    private List<AddressesRest> addresses;
 }
